d7c16b1020b27cc4f2331faf46500250b61e0e32
grimsfreshacc-blip <grimsfreshacc@gmail.com>
2025-12-05 02:04:55 -0500

    Update epic.js

--- src/epic.js
+++ src/epic.js
@@ -1,39 +1,56 @@
 import axios from "axios";
 
-// EXCHANGE EPIC CODE FOR ACCESS TOKEN
-export async function exchangeCode(code) {
-  const res = await axios.post(
-    "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
-    `grant_type=authorization_code&code=${code}`,
-    { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+const EPIC_TOKEN = "basic ZWM2ZjRhZGZiOTRkND..."; // unchanged default fortnite launcher token
+
+export async function startDeviceAuth() {
+  const response = await axios.post(
+    "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/deviceAuthorization",
+    new URLSearchParams({}),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": EPIC_TOKEN
+      }
+    }
   );
 
-  return res.data.access_token;
+  return response.data;
 }
 
-// GET LOCKER WITH ACCESS TOKEN
-export async function getLocker(accessToken) {
-  const res = await axios.get(
-    "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/accountId/client/QueryProfile?profileId=athena",
+export async function pollDeviceAuth(deviceCode) {
+  try {
+    const res = await axios.post(
+      "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
+      new URLSearchParams({
+        grant_type: "device_code",
+        device_code: deviceCode
+      }),
+      { headers: { Authorization: EPIC_TOKEN } }
+    );
+
+    return res.data.access_token;
+
+  } catch (err) {
+    if (err.response?.data?.error === "authorization_pending")
+      return "authorization_pending";
+
+    throw err;
+  }
+}
+
+export async function getLockerData(accessToken) {
+  const response = await axios.get(
+    "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/lookup",
     { headers: { Authorization: `bearer ${accessToken}` } }
   );
 
-  const items = res.data.profileChanges[0].profile.items;
+  const accountId = response.data.id;
 
-  return {
-    skin: findItem(items, "AthenaCharacter"),
-    backbling: findItem(items, "AthenaBackpack"),
-    pickaxe: findItem(items, "AthenaPickaxe"),
-    glider: findItem(items, "AthenaGlider"),
-    emote: findItem(items, "AthenaDance"),
-    exclusive: Object.keys(items)
-      .filter(id => items[id].attributes?.rarity === "EFortRarity::Mythic")
-  };
-}
-
-function findItem(items, type) {
-  const entry = Object.entries(items).find(
-    ([id, data]) => data.templateId.includes(type)
+  const locker = await axios.post(
+    `https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/${accountId}/client/QueryProfile?profileId=athena`,
+    {},
+    { headers: { Authorization: `bearer ${accessToken}` } }
   );
-  return entry ? entry[0] : null;
+
+  return locker.data;
 }
