d1912d203e39ce9d6656320deadd52184ba23c76
grimsfreshacc-blip <grimsfreshacc@gmail.com>
2025-12-05 02:15:02 -0500

    Update fetchCosmeticImage.js

--- src/fetchCosmeticImage.js
+++ src/fetchCosmeticImage.js
@@ -1,1 +1,7 @@
-
+export function getImage(cosmetic) {
+    return (
+        cosmetic?.images?.icon ||
+        cosmetic?.images?.featured ||
+        "https://via.placeholder.com/256?text=No+Image"
+    );
+}
