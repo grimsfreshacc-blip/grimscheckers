21347409330679719a1a25515b6496662439be24
grimsfreshacc-blip <grimsfreshacc@gmail.com>
2025-12-05 22:52:20 -0500

    Delete src directory

--- src/fetchCosmeticImage.js
+++
@@ -1,8 +0,0 @@
-export async function getCosmeticImage(templateId) {
-    const shortId = templateId.split(":")[1];
-
-    return {
-        id: shortId,
-        icon: `https://fortnite-api.com/images/cosmetics/br/${shortId}/icon.png`
-    };
-}
